17f783d1867a428f8a7171b90ad74c0b5c0a29ff
root <root@LAPTOP-1RT2KH5U>
2024-11-06 13:55:34 -0700

    database creation and insertion templates

---
+++ ProjectSourceCode/src/init_data/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password CHAR(100) NOT NULL,
+    profile_img VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    item_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    item_img VARCHAR(100),
+    color VARCHAR(100),
+    price FLOAT,
+    formality VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS wishlist (
+    id_user INT NOT NULL,
+    id_item INT NOT NULL
+);
