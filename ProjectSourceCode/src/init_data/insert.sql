e0fa3f95008a42ab95e8f21f0fb1277462acb87c
root <root@LAPTOP-1RT2KH5U>
2024-11-06 13:55:34 -0700

    database creation and insertion templates

---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (user_id, username, password, profile_img) VALUES
+(1, 'jessika25', 'longhashedpassword123','/user1img.jpg');
+
+INSERT INTO items (item_id, name, item_img, color, price, formality) VALUES
+(1, 'Nordstrom Black Tuxedo', '/tuxedo.png', 'black', 149.99, "formal");
+
+INSERT INTO wishlist (id_user, id_item) VALUES
+(1,1);
